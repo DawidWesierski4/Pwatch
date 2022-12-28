25d93dd30bab833a9d08bcc41270cb2a85809d44
DawidWesierski4 <dawidwesierski4@gmail.com>
2022-12-28 15:39:25 +0100

    add pwatch_readStat prototype

--- src/pwatch.h
+++ src/pwatch.h
@@ -2,25 +2,33 @@
 #define PWATCH_H
 
 #include <stdint.h>
+#include <stdbool.h>
+#include <pthread.h>
 
-#define PWATCH_LINE_LIMIT 256
+#define PWATCH_LINE_LIMIT 26
 #define BIT(n) (0x1U << (n))
 
-
 enum pwatch_errNum {
-    PWATCH_OPERATION_SUCCESSFUL,
+    PWATCH_SUCCESS,
     PWATCH_ERR_FAILED_TO_OPEN,
     PWATCH_ERR_FAILED_TO_READ,
-    PWATCH_ERR_WRONG_ADMIN_FLAG
+    PWATCH_ERR_FAILED_TO_ACQUIRE_MEMORY,
+    PWATCH_ERR_FAILED_TO_PROCESS_ADMIN_FLAG,
+    PWATCH_ERR_FAILED_TO_PROCESS_SIGNAL
 };
 
 enum pwatch_adminFlagsNum {
-    PWATCH_ADMIN_READ       = BIT(0)
+    PWATCH_ADMIN_READ           = BIT(0),
+    PWATCH_ADMIN_PROCESS        = BIT(1),
+    PWATCH_ADMIN_READ_CLOSED    = BIT(30),
+    PWATCH_ADMIN_CLOSE          = BIT(31)
 };
 
-extern uint32_t pwatch_adminFlags;
-
-const char *pwatch_procStatPath = "/proc/stat";
+typedef struct pwatch_queueLineBuf
+{
+    char* line;
+    void* next;
+} pwatch_queueLineBuf;
 
 typedef struct pwatch_cpuStat {
     int user;       /* normal processes executing in user mode */
@@ -32,7 +40,36 @@ typedef struct pwatch_cpuStat {
     int softirq;    /* servicing softirqs */
 } pwatch_cpuStat;
 
-int pwatch_readStat(void);
+typedef struct pwatch_cpuStatList {
+    int number;
+    void* node;
+    void* next;
+    void* back;
+} pwatch_cpuStatList;
+
+typedef struct pwatch_semaphore {
+    int nmb;
+    bool fullFlg;
+    pthread_cond_t full;
+    bool emptyFlg;
+    pthread_cond_t empty;
+} pwatch_semaphore;
+
+extern pwatch_semaphore *readSemaphore;
+
+extern pwatch_queueLineBuf *readBuff;
+
+extern uint32_t adminFlags;
+
+extern uint32_t cpuStatList;
+
+extern pthread_mutex_t lock;
+
+const char *procStatPath = "/proc/stat";
+
+int pwatch_parseStat (const char* line, int len);
+
+int pwatch_readStat (void);
 
 const char* pwatch_errToString (int errEnum);
 
--- src/pwatch_main.c
+++ src/pwatch_main.c
@@ -3,9 +3,22 @@
 #include <string.h>
 #include <stdint.h>
 #include <time.h>
+#include <unistd.h>
 #include "pwatch.h"
 
-uint32_t pwatch_adminFlags = 0;
+uint32_t adminFlags;
+pthread_mutex_t lock;
+
+pwatch_queueLineBuf* readBuff;
+
+pwatch_semaphore *readSemaphore;
+
+pwatch_cpuStatList cpuStats = {
+    .number = 0,
+    .node = NULL,
+    .next = NULL,
+    .back = NULL
+};
 
 /**
   * pwatch_errToString
@@ -19,10 +32,43 @@ const char* pwatch_errToString (int errEnum)
             return "PWATCH_ERR_FAILED_TO_OPEN";
         case PWATCH_ERR_FAILED_TO_READ:
             return "PWATCH_ERR_FAILED_TO_READ";
-        case PWATCH_ERR_WRONG_ADMIN_FLAG:
-            return "PWATCH_ERR_WRONG_ADMIN_FLAG";
+        case PWATCH_ERR_FAILED_TO_ACQUIRE_MEMORY:
+            return "PWATCH_ERR_FAILED_TO_ACQUIRE_MEMORY";
+        case PWATCH_ERR_FAILED_TO_PROCESS_ADMIN_FLAG:
+            return "PWATCH_ERR_FAILED_TO_PROCESS_ADMIN_FLAG";
+        case PWATCH_ERR_FAILED_TO_PROCESS_SIGNAL:
+            return "PWATCH_ERR_FAILED_TO_PROCESS_SIGNAL";
         default:
             return "PWATCH_ERR_UNKNOWN_ERROR";
+    }
+}
+
+/**
+  * pwatch_parseStat
+  *     parse / fill the pwatch_cpuStat
+ **/
+int pwatch_parseStat(const char* line, int len)
+{
+    printf("line %s length %d \n", line, len);
+    // pwatch_queueLineBuf aux;
+    // while (!pwatch_semaphore.empty) {
+
+    //     printf("line %s length %d \n", line, len);
+    // }
+    return PWATCH_SUCCESS;
+}
+
+/**
+  * releaseQueueLineBuf
+  *     release the reading queue
+ **/
+void pwatch_releaseQueueLineBuf(void)
+{
+    pwatch_queueLineBuf *aux;
+    while (readBuff != NULL) {
+        aux = readBuff;
+        readBuff = readBuff->next;
+        free(aux);
     }
 }
 
@@ -32,51 +78,93 @@ const char* pwatch_errToString (int errEnum)
  **/
 int pwatch_readStat(void)
 {
-    FILE *procStat;
-    pwatch_cpuStat* cpus = malloc(sizeof(pwatch_cpuStat));
-    procStat = fopen(pwatch_procStatPath, "r");
-    char *line = malloc(sizeof(char) * PWATCH_LINE_LIMIT);
+    pwatch_queueLineBuf *lineStruct = readBuff;
+    FILE *procStat = fopen(procStatPath, "r");
     size_t len = 0;
-    ssize_t ret;
+    ssize_t ret_line_size; // we set it to to went into while reading
 
-    cpus->idle = 0; //TODO interface for the read function
-
-    if (procStat == NULL)
-        return PWATCH_ERR_FAILED_TO_OPEN;
-
-    if (pwatch_adminFlags & PWATCH_ADMIN_READ) {
-        ret = getline(&line, &len, procStat);
-    else
-        return PWATCH_ERR_WRONG_ADMIN_FLAG;
-
-        if (fseek(procStat, ret, SEEK_SET) != 0) {
+    while (true) {
+        if(adminFlags & PWATCH_ADMIN_CLOSE) {
             fclose(procStat);
-            return PWATCH_ERR_FAILED_TO_READ;
+            pwatch_releaseQueueLineBuf();
+            adminFlags &= ~PWATCH_ADMIN_READ;
+            adminFlags &= ~PWATCH_ADMIN_READ_CLOSED;
+            return PWATCH_SUCCESS;
         }
 
-        pwatch_parseMail(red, );
+        /* it comes here when you stop reading in case of timeout f.e.*/
+        if(!(adminFlags & PWATCH_ADMIN_READ))
+            continue;
 
-        
-    } else {
+        if (procStat == NULL)
+            return PWATCH_ERR_FAILED_TO_OPEN;
 
+        // ret_line_size = getline(&line, &len, procStat);
+
+        // while (ret_line_size < 0)
+        // free(line);
+        // line = NULL;
+        // len = 0;
+        // ret_line_size = getline(&line, &len, procStat);
+        // fclose(procStat);
+
+        ret_line_size = getline(&lineStruct->line, &len, procStat);
+        if (feof(procStat)) {
+            fclose(procStat);
+            procStat = fopen(procStatPath, "r");
+            continue;
+        } else if (ret_line_size < 0)
+            return PWATCH_ERR_FAILED_TO_READ;
+
+        lineStruct->next = (pwatch_queueLineBuf*)malloc(sizeof(pwatch_queueLineBuf));
+        if (lineStruct->next == NULL)
+            return PWATCH_ERR_FAILED_TO_ACQUIRE_MEMORY;
+
+        lineStruct = (pwatch_queueLineBuf*) lineStruct->next;
+        lineStruct->line = NULL;
+        lineStruct->next = NULL;
+
+        if (ret_line_size <= 0)
+            return PWATCH_ERR_FAILED_TO_READ;
+
+        pthread_mutex_lock(&lock);
+        readSemaphore->nmb++;
+        if (readSemaphore->nmb >= PWATCH_LINE_LIMIT) {
+            readSemaphore->fullFlg = true;
+            if (!pthread_cond_wait(&readSemaphore->full, &lock))
+                return PWATCH_ERR_FAILED_TO_PROCESS_SIGNAL;
+        } else if (readSemaphore->nmb == 1){
+            readSemaphore->emptyFlg = false;
+            pthread_cond_signal(&readSemaphore->empty);
+        }
+        pthread_mutex_unlock(&lock);
     }
-
-    fclose(procStat);
-    return PWATCH_OPERATION_SUCCESSFUL;
 }
 
 int main(int argc, char** argv)
 {
     int ret;
+    readBuff = malloc(sizeof(pwatch_queueLineBuf));
+    readBuff->line = NULL;
+    readBuff->next = NULL;
+    readSemaphore = malloc(sizeof(pwatch_semaphore));
+    readSemaphore->nmb = 0;
+    readSemaphore->fullFlg = false;
+    readSemaphore->emptyFlg = true;
+
+    adminFlags = 0;
+
 
     if(argc == 2 && strcmp(argv[0], "debug"))
         printf("Initialization \n");
 
-    pwatch_adminFlags |= PWATCH_ADMIN_READ;
+    adminFlags |= PWATCH_ADMIN_READ;
+    adminFlags |= PWATCH_ADMIN_PROCESS;
+    ret = pthread_cond_init(&readSemaphore->empty, NULL);
+    ret = pthread_cond_init(&readSemaphore->full, NULL);
 
-    ret = pwatch_readStat();
-    if (!ret) {
-        fprintf(stderr, pwatch_errToString(ret), "\n");
+    if ((ret = pwatch_readStat())) {
+        fprintf(stderr, "%s \n", pwatch_errToString(ret));
         return ret;
     }
 
